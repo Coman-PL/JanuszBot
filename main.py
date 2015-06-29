98848ea6a9ccff5406c08477b3613ec53929ec09
Piotr Manel <piotr.manel@industi.com>
2015-06-29 10:53:01 +0000

    Ap ID

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 import webapp2
 
-TOKEN = 'YOUR_BOT_TOKEN_HERE'
+TOKEN = '106332547:AAHsvxS1YvLxJbLgFZ_Nc77QcWrkcvV9oTI'
 
 BASE_URL = 'https://api.telegram.org/bot' + TOKEN + '/'
 
